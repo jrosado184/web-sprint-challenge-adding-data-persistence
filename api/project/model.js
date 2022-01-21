c93ad29a946883999a9a80caf24eab6792d18593
jrosado184 <88807540+jrosado184@users.noreply.github.com>
2022-01-21 16:17:45 -0500

    boolean convert for projects

--- api/project/model.js
+++ api/project/model.js
@@ -1,8 +1,19 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig");
 
-const getAll = () => {
-  return db("projects");
+const getAll = async () => {
+  const rows = await db("projects");
+  const newRow = rows.map((row) => {
+    const rowsNew = {
+      project_id: row.project_id,
+      project_name: row.project_name,
+      project_description: row.project_description,
+      project_completed: row.project_completed === 1 ? true : false,
+    };
+    return rowsNew;
+  });
+
+  return newRow;
 };
 
 const create = async (project) => {
